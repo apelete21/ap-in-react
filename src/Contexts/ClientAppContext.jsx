3281a8e4d6eb80f6ee18fa0462cdf9d4d5d978fb
Apelete <adzohonouapelete@gmail.com>
2023-06-01 20:23:17 +0000

    visits

--- src/Contexts/ClientAppContext.jsx
+++ src/Contexts/ClientAppContext.jsx
@@ -1,47 +1,55 @@
-import React, { createContext, useEffect, useState } from 'react'
-import { useLocation } from 'react-router-dom';
-import Loader from '../service/loader';
+import React, { createContext, useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import Loader from "../service/loader";
+import { visitController } from "../service/visits";
 
-export const ClientAppContext = createContext()
+export const ClientAppContext = createContext();
 
 export const ClientAppContextProvider = ({ children }) => {
+  let [jobSelected, setJobSelected] = useState({});
+  const { pathname } = useLocation();
 
-    let [jobSelected, setJobSelected] = useState({})
-    const { pathname } = useLocation()
+  function openNav() {
+    Loader();
+    setTimeout(() => {
+      document.getElementById("main--nav").style.width = "100%";
+    }, 400);
+  }
 
-    function openNav() {
-        Loader()
-        setTimeout(() => {
-            document.getElementById("main--nav").style.width = "100%";
-        }, 400);
-    }
+  function closeNav() {
+    Loader();
+    setTimeout(function () {
+      document.getElementById("main--nav").style.width = "0%";
+    }, 400);
+  }
 
-    function closeNav() {
-        Loader()
-        setTimeout(function () {
-            document.getElementById("main--nav").style.width = "0%";
-        }, 400);
-    }
+  const SelectingJob = (element) => {
+    setJobSelected(element);
+  };
 
-    const SelectingJob = (element) => {
-        setJobSelected(element)
-    }
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
 
-    useEffect(() => {
-        window.scrollTo(0, 0)
-    }, [pathname])
+  useEffect(() => {
+    const visits = async () => {
+      const token = localStorage.getItem("appToken");
+      const { newToken } = await visitController({ page: "appToken", token });
+      if (newToken) return localStorage.setItem("appToken", newToken);
+    };
+    visits()
+  }, []);
 
-
-    return (
-        <ClientAppContext.Provider
-            value={{
-                openNav,
-                closeNav,
-                jobSelected,
-                SelectingJob
-            }}
-        >
-            {children}
-        </ClientAppContext.Provider>
-    )
-}
+  return (
+    <ClientAppContext.Provider
+      value={{
+        openNav,
+        closeNav,
+        jobSelected,
+        SelectingJob,
+      }}
+    >
+      {children}
+    </ClientAppContext.Provider>
+  );
+};
---
+++ src/service/visits.js
@@ -0,0 +1,11 @@
+import { baseUrl } from "../api/url";
+
+/**
+ * @param {string} page
+ * @param {string} token
+ * @returns {null}
+ */
+export async function visitController({ page, token }) {
+  const res = await fetch(`${baseUrl}/visits?page=${page}&token=${token}`);
+  return await res.json()
+}
