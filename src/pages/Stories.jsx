ff54d3249f7844db987ac1c5dba2fd27178d5bea
Apelete <adzohonouapelete@gmail.com>
2023-06-12 16:42:06 -0700

    update

--- src/pages/Stories.jsx
+++ src/pages/Stories.jsx
@@ -16,16 +16,16 @@ export default function Stories() {
   return (
     <>
       <Helmet>
-        <title>Ours stories | AP’IN</title>
+        <title>Ours stories | AP'IN</title>
       </Helmet>
       <Motion>
         <header>
-          <div class="fluid-wrapper bg_white">
+          <div className="fluid-wrapper bg_white">
             <MenuNav logoImage={icons.lgDark} linkView={"darklink"} />
 
             <MenuButtonDark />
-            <div class="offset-canva story-main-title">
-              <h1 class="section--hero__title">
+            <div className="offset-canva story-main-title">
+              <h1 className="section--hero__title">
                 This is <br />
                 where we tell stories
               </h1>
@@ -41,87 +41,87 @@ export default function Stories() {
                START OF HERO SECTION
         ===========================--> */}
 
-        <section class="story--intro mb-lg offset-canva">
-          <div class="stories_links">
-            <ul class="links_list">
-              <li class="link_list_item link_item--active"> ALL </li>
-              <li class="link_list_item"> PODCAST </li>
-              <li class="link_list_item"> BLOG </li>
-              <li class="link_list_item"> PRESS RELEASE </li>
+        <section className="story--intro mb-lg offset-canva">
+          <div className="stories_links">
+            <ul className="links_list">
+              <li className="link_list_item link_item--active"> ALL </li>
+              <li className="link_list_item"> PODCAST </li>
+              <li className="link_list_item"> BLOG </li>
+              <li className="link_list_item"> PRESS RELEASE </li>
             </ul>
           </div>
 
-          <div class="stories_items_container">
-            <div class="stories_item">
-              <div class="type_of_job">Digital Marketing</div>
-              <h6 class="story_title">
+          <div className="stories_items_container">
+            <div className="stories_item">
+              <div className="type_of_job">Digital Marketing</div>
+              <a href="#" className="story_title">
                 What's the best <br /> performance fee structure <br /> for
                 hedge funds?
-              </h6>
-              <div class="story_details">
-                <div class="story_date">Avr, 12 2022</div>
-                <div class="story_author">By John Doe</div>
+              </a>
+              <div className="story_details">
+                <div className="story_date">Avr, 12 2022</div>
+                <div className="story_author">By John Doe</div>
               </div>
             </div>
-            <div class="stories_item stories_item-img_bg">
-              <div class="type_of_job">Digital Marketing</div>
-              <h6 class="story_title">
+            <div className="stories_item stories_item-img_bg">
+              <div className="type_of_job">Digital Marketing</div>
+              <a href="#" className="story_title">
                 What's the best <br /> performance fee structure <br /> for
                 hedge funds?
-              </h6>
-              <div class="story_details">
-                <div class="story_date">Avr, 12 2022</div>
-                <div class="story_author">By John Doe</div>
+              </a>
+              <div className="story_details">
+                <div className="story_date">Avr, 12 2022</div>
+                <div className="story_author">By John Doe</div>
               </div>
             </div>
-            <div class="stories_item">
-              <div class="type_of_job">Digital Marketing</div>
-              <h6 class="story_title">
+            <div className="stories_item">
+              <div className="type_of_job">Digital Marketing</div>
+              <a href="#" className="story_title">
                 What's the best <br /> performance fee structure <br /> for
                 hedge funds?
-              </h6>
-              <div class="story_details">
-                <div class="story_date">Avr, 12 2022</div>
-                <div class="story_author">By John Doe</div>
+              </a>
+              <div className="story_details">
+                <div className="story_date">Avr, 12 2022</div>
+                <div className="story_author">By John Doe</div>
               </div>
             </div>
-            <div class="stories_item stories_item-bg_red">
-              <div class="type_of_job">Digital Marketing</div>
-              <h6 class="story_title">
+            <div className="stories_item stories_item-bg_red">
+              <div className="type_of_job">Digital Marketing</div>
+              <a href="#" className="story_title">
                 What's the best <br /> performance fee structure <br /> for
                 hedge funds?
-              </h6>
-              <div class="story_details">
-                <div class="story_date">Avr, 12 2022</div>
-                <div class="story_author">By John Doe</div>
+              </a>
+              <div className="story_details">
+                <div className="story_date">Avr, 12 2022</div>
+                <div className="story_author">By John Doe</div>
               </div>
             </div>
-            <div class="stories_item">
-              <div class="type_of_job">Digital Marketing</div>
-              <h6 class="story_title">
+            <div className="stories_item">
+              <div className="type_of_job">Digital Marketing</div>
+              <a href="#" className="story_title">
                 What's the best <br /> performance fee structure <br /> for
                 hedge funds?
-              </h6>
-              <div class="story_details">
-                <div class="story_date">Avr, 12 2022</div>
-                <div class="story_author">By John Doe</div>
+              </a>
+              <div className="story_details">
+                <div className="story_date">Avr, 12 2022</div>
+                <div className="story_author">By John Doe</div>
               </div>
             </div>
-            <div class="stories_item">
-              <div class="type_of_job">Digital Marketing</div>
-              <h6 class="story_title">
+            <div className="stories_item">
+              <div className="type_of_job">Digital Marketing</div>
+              <a href="#" className="story_title">
                 What's the best <br /> performance fee structure <br /> for
                 hedge funds?
-              </h6>
-              <div class="story_details">
-                <div class="story_date">Avr, 12 2022</div>
-                <div class="story_author">By John Doe</div>
+              </a>
+              <div className="story_details">
+                <div className="story_date">Avr, 12 2022</div>
+                <div className="story_author">By John Doe</div>
               </div>
             </div>
           </div>
-          <div class="load_more_btn">
-            <a class="btn default-outline" href="">
-              Load more
+          <div className="load_more_btn">
+            <a className="btn default-outline" href="">
+              Load more...
               <span>
                 <img src="" alt="" />
               </span>
