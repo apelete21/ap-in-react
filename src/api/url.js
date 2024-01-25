006c9b74c75a9541ee874593cd7d05cb4978fae4
ADZOHONOU K. Apelete <87383047+apelete21@users.noreply.github.com>
2024-01-25 16:15:13 +0000

    Update url.js

--- src/api/url.js
+++ src/api/url.js
@@ -1,3 +1,3 @@
 // api request url
-export const baseUrl = "https://ap-in-api-flax.vercel.app/api/v1";
+export const baseUrl = "https://apin-oabu.onrender.com/api/v1";
 // export const baseUrl = "http://localhost:5000/api/v1";
